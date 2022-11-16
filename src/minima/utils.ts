f6d4132c62898042741c1ef705ddc6f0689c6a60
Elias <eliasnemr@gmail.com>
2022-11-16 14:25:12 +0200

    Tweaks
    + check if tokenid is Minima on Coinsplit and execute correctly
    + v1.13.8

--- src/minima/utils.ts
+++ src/minima/utils.ts
@@ -1,3 +1,4 @@
+import { MINIMA__TOKEN_ID } from './../shared/constants';
 /**
  * 
  * Minima Utility Functions
@@ -17,11 +18,17 @@ export const splitCoin = async (tokenid: string, sendable: string, burn: string)
   const mAddr = fetchAddr.response.miniaddress;
 
   let finalTotalToSend = new Decimal(0);
-  if (burn.length > 0) {
+  if (burn.length > 0 && tokenid === MINIMA__TOKEN_ID) {
     // minus the burn, get total to send back after splitting
     finalTotalToSend = new Decimal(sendable).minus(new Decimal(burn));
     console.log(finalTotalToSend.toString())
     return rpc(`send address:${mAddr} tokenid:${tokenid} amount:${finalTotalToSend.toString()} split:${2} burn:${burn}`);
+  }
+
+  if (burn.length > 0 && tokenid !== MINIMA__TOKEN_ID) {
+    return rpc(`send address:${mAddr} tokenid:${tokenid} amount:${sendable} split:${2} burn:${burn}`);
+
+
   }
 
   return rpc(`send address:${mAddr} tokenid:${tokenid} amount:${sendable} split:${2}`);  
