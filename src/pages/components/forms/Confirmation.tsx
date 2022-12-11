a32928f9be95dd096ba3c6f89a484100974516b5
Elias <eliasnemr@gmail.com>
2022-12-11 12:24:07 +0000

    Confirmation layout corrections

--- src/pages/components/forms/Confirmation.tsx
+++ src/pages/components/forms/Confirmation.tsx
@@ -11,36 +11,40 @@ const Confirmation = ({ title, children, closeFn, open, formik, modalTitle }: an
     return (
         <Modal open={open} onClose={closeFn} onBackdropClick={closeFn} className={styles['burn-modal-wrapper']}>
             <Box className={styles['burn-modal']}>
-                <Box component="div">
-                    <Toolbar sx={{ pl: '16px!important' }} variant="dense" className={styles['burn-modal-hdr']}>
-                        <Typography aria-label="modal-title" className={styles['burn-modal-title']} variant="h6">
-                            {modalTitle && modalTitle.length ? modalTitle : 'Confirmation'}
-                        </Typography>
-                    </Toolbar>
-                    <Stack direction="column" sx={{ padding: '8px 16px' }}>
+                <Toolbar variant="dense" className={styles['burn-modal-hdr']}>
+                    <Typography aria-label="modal-title" className={styles['burn-modal-title']} variant="h6">
+                        {modalTitle && modalTitle.length ? modalTitle : 'Confirmation'}
+                    </Typography>
+                </Toolbar>
+                <Box component="div" className={styles['content']}>
+                    <Stack direction="column" sx={{ padding: '8px 16px', overflow: 'auto', height: '100%' }}>
                         {children}
+                        <ModalButtonWrapper
+                            children={
+                                <>
+                                    <Button
+                                        disabled={formik.isSubmitting}
+                                        variant="outlined"
+                                        color="inherit"
+                                        onClick={closeFn}
+                                    >
+                                        Cancel
+                                    </Button>
+                                    <Button
+                                        disabled={formik.isSubmitting}
+                                        variant="contained"
+                                        disableElevation
+                                        color="primary"
+                                        className={styles['burn-skip-btn']}
+                                        onClick={() => formik.handleSubmit()}
+                                    >
+                                        {formik.isSubmitting ? 'Please wait...' : 'Accept'}
+                                    </Button>
+                                </>
+                            }
+                        />
                     </Stack>
                 </Box>
-
-                <ModalButtonWrapper
-                    children={
-                        <>
-                            <Button disabled={formik.isSubmitting} variant="outlined" color="inherit" onClick={closeFn}>
-                                Cancel
-                            </Button>
-                            <Button
-                                disabled={formik.isSubmitting}
-                                variant="contained"
-                                disableElevation
-                                color="primary"
-                                className={styles['burn-skip-btn']}
-                                onClick={() => formik.handleSubmit()}
-                            >
-                                {formik.isSubmitting ? 'Please wait...' : 'Accept'}
-                            </Button>
-                        </>
-                    }
-                />
             </Box>
         </Modal>
     );
--- src/pages/components/managers/ModalManager.tsx
+++ src/pages/components/managers/ModalManager.tsx
@@ -1,4 +1,3 @@
-import Burn from '../forms/Burn';
 import Confirmation from '../forms/Confirmation';
 
 const ModalManager = ({ closeFn, modal, children, formik }: any) => {
