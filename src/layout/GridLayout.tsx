68e6c7012172b796d253d023d6656ed1007b3c61
Elias <eliasnemr@gmail.com>
2023-01-31 13:53:40 +0200

    Fixed layout

--- src/layout/GridLayout.tsx
+++ src/layout/GridLayout.tsx
@@ -2,7 +2,7 @@ import { CircularProgress, Grid } from '@mui/material';
 
 const GridLayout = ({ children, loading }: any) => {
     return (
-        <Grid container sx={{ paddingBottom: 4 }}>
+        <Grid container>
             <Grid item xs={0} md={2} />
             <Grid item xs={12} md={8} className={!loading ? '' : 'center'}>
                 {!loading ? children : <CircularProgress size={32} />}
