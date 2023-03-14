f9ff5d261e5f8ff465f8ddb6c919ae0c7f2c01ab
Elias <eliasnemr@gmail.com>
2023-03-14 12:08:55 +0200

    Check valid true for web validation

--- src/minima/commands/tokenValidate/index.ts
+++ src/minima/commands/tokenValidate/index.ts
@@ -3,7 +3,7 @@ export const tokenValidate = (tokenid: string) => {
         MDS.cmd(`tokenvalidate tokenid:${tokenid}`, (r) => {
             if (!r.status) reject(r.error ? r.error : 'RPC Failed');
 
-            if (r.response.web.webvalidate) resolve(true);
+            if (r.response.web.valid) resolve(true);
         });
     });
 };
