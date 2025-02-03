fed8e39f80981766a3b35bc1646c4bd6af5d9a1e
dynamitesushi <winson.hero@gmail.com>
2025-02-03 20:06:01 +0000

    added translation for nav

--- src/routes/currency-display/index.tsx
+++ src/routes/currency-display/index.tsx
@@ -3,12 +3,14 @@ import { createFileRoute } from '@tanstack/react-router'
 import Header from '../../components/Header'
 import Navigation from '../../components/Navigation'
 import { appContext } from '../../AppContext'
+import useTranslation from '../../hooks/useTranslation'
 
 export const Route = createFileRoute('/currency-display/')({
   component: Index,
 })
 
 function Index() {
+  const { t } = useTranslation();
   const { currencyType, setCurrencyType } = useContext(appContext)
 
   const OPTIONS = [
@@ -60,12 +62,12 @@ function Index() {
             <Navigation />
           </div>
           <div className="grow flex flex-col">
-            <h1 className="text-white text-2xl mb-6">Choose a display format</h1>
+            <h1 className="text-white text-2xl mb-6">{t("choose_a_display_format")}</h1>
             <div className="flex flex-col gap-3">
               {OPTIONS.map((option) => (
-                <div className="bg-grey10 dark:bg-darkContrast relative w-full flex p-4 rounded">
-                  <label htmlFor={option.value} className="flex items-center gap-5 cursor-pointer">
-                    <input id={option.value} name="currency-display" type="radio" className="peer hidden" value={option.key} checked={currencyType === option.key} onChange={handleChange} />
+                <div key={option.key} className="bg-grey10 dark:bg-darkContrast relative w-full flex p-4 rounded">
+                  <label htmlFor={option.key} className="flex items-center gap-5 cursor-pointer">
+                    <input id={option.key} name="currency-display" type="radio" className="peer hidden" value={option.key} checked={currencyType === option.key} onChange={handleChange} />
                     <div className="relative peer-checked:[&>div]:opacity-100 w-4 h-4 border-2 border-grey60 peer-checked:border-orange rounded-full flex items-center justify-center">
                       <div className="absolute inset-0 opacity-0 top-0.5 left-0.5 w-2 h-2 rounded-full bg-orange" />
                     </div>
