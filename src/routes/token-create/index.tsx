8eb6414f61c53af4f54eb6613ac46e9dda40e231
dynamitesushi <winson.hero@gmail.com>
2025-02-12 17:21:05 +0000

    refined select image

--- src/routes/token-create/index.tsx
+++ src/routes/token-create/index.tsx
@@ -81,7 +81,7 @@ function Index() {
             </div>
 
             {selectedToken === "UPLOAD_IMAGE" && (
-              <label className="cursor-pointer mt-2 bg-contrast1 hover:bg-contrast1.5 py-10 text-grey60">
+              <label className={`cursor-pointer mt-2 bg-contrast1 py-10 text-grey60 ${!image ? 'hover:bg-contrast1.5' : ''}`}>
                 <div className="flex flex-col items-center gap-4">
                   {!image && (
                     <div className="flex flex-col items-center gap-4">
@@ -104,8 +104,8 @@ function Index() {
                         alt="Token preview"
                         className="object-contain"
                       />
-                      <div className="absolute z-[20] right-4 bottom-4">
-                        <div onClick={removeImage} className="bg-darkContrast rounded-full p-2">
+                      <div className="absolute z-[20] right-3 bottom-3">
+                        <div onClick={removeImage} className="border border-contrast2 bg-contrast1 hover:bg-contrast2 rounded-full h-[36px] w-[36px] flex items-center justify-center">
                           <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M2.6155 16C2.168 16 1.78683 15.8426 1.472 15.528C1.15733 15.2131 1 14.832 1 14.3845V1.99996H0V0.999963H4V0.230713H10V0.999963H14V1.99996H13V14.3845C13 14.8448 12.8458 15.2291 12.5375 15.5375C12.2292 15.8458 11.8448 16 11.3845 16H2.6155ZM12 1.99996H2V14.3845C2 14.564 2.05767 14.7115 2.173 14.827C2.2885 14.9423 2.436 15 2.6155 15H11.3845C11.5385 15 11.6796 14.9359 11.8077 14.8077C11.9359 14.6795 12 14.5385 12 14.3845V1.99996ZM4.80775 13H5.80775V3.99996H4.80775V13ZM8.19225 13H9.19225V3.99996H8.19225V13Z" fill="#E9E9EB" />
                           </svg>
@@ -302,7 +302,7 @@ const Dropdown = ({ options, value, onChange }: DropdownProps) => {
         </div>
       </div>
 
-      <div className={`fixed top-0 left-0 w-full h-full bg-black z-[20] lg:z-[10] ${isOpen ? 'opacity-80' : 'opacity-0 pointer-events-none'}`}></div>
+      <div onClick={toggleDropdown} className={`fixed top-0 left-0 w-full h-full bg-black z-[20] lg:z-[10] ${isOpen ? 'opacity-80' : 'opacity-0 pointer-events-none'}`}></div>
     </div>
   )
 }
